c286998dd9e580b7d5d1cb5d11cd6d94e6946123
Ethan Madison <ethan@ethanmad.com>
2015-03-25 23:26:41 -0700

    Added TODO (read from file)

--- download.py
+++ download.py
@@ -1,3 +1,3 @@
 import urllib
-
+# TODO: read in a text file containing file urls and file names, then download all files listed
 urllib.urlretrieve("http://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=77w2", "chrome-beta.apk")
