f53606183f7bc21ba8a6d54fd61ee275ce1468aa
Ethan Madison <ethan@ethanmad.com>
2015-03-25 23:23:23 -0700

    Removed old code

--- parser.py
+++ parser.py
@@ -16,15 +16,13 @@ def get_postarea(response):
 def get_url(string_to_split):
   return string.split(str(string_to_split), '"')[3]
 
-# find url of newest upload
+# find url of newest upload page
 response = load_page("http://www.apkmirror.com/apk/google-inc/chrome/")
 download = get_postarea(response).findAll("a", { "class" : "downloadLink"})[0]
-# url = string.split(str(download), '"')[3]
 url =  "http://www.apkmirror.com" + get_url(download)
 
 # find actual download link for newest upload
 response2 = load_page(url)
 download2 = get_postarea(response2).findAll("a", { "type": "button" })[0]
-# download_link = string.split(str(download2), '"')[3]
 file_url = get_url(download2)
-print file_url 
+print file_url
